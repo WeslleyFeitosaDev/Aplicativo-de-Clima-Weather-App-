62f28fcdb94a63127998217416e77567ecf4bb50
Weslley Feitosa <weslleybarbosa662@gmail.com>
2025-06-09 16:16:15 -0300

    Update script.js

--- conteudo/script.js
+++ conteudo/script.js
@@ -6,6 +6,7 @@ let cidade=document.getElementById("cidade");
 let weather=document.getElementById("weather");
 let mensagem_error=document.getElementById("mensagem_error");
 let bx_cloud_sun=document.getElementById("bx-cloud-sun");
+let bx_badge_info=document.getElementById("bx-badge-info");
 
 bnt_verificar_cidade.addEventListener("click",()=>{
     bx_cloud_sun.style.display="none";
@@ -19,9 +20,20 @@ bnt_verificar_cidade.addEventListener("click",()=>{
         .then(Response =>{
             if(!Response.ok){
                 mensagem_error.innerHTML="Cidade não encontrada";
+                mensagem_error.classList.add("mensagem_error_cidade_n_encontrada");
+                bx_badge_info.style.display="flex";
+                cidade.innerHTML = "";
+                weather.innerHTML = "";
+
+            }else{
+                mensagem_error.innerHTML=""
+                mensagem_error.classList.remove("mensagem_error_cidade_n_encontrada");
+                bx_badge_info.style.display="none";
             }
+            return Response.json();
         })
         .then(data => {
+            
             const name=data.name;
             const temp=data.main.temp;
             const description=data.weather[0].description;
