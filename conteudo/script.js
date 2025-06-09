bc0d16090de1285adb81aefe2962ef5340160198
Weslley Feitosa <weslleybarbosa662@gmail.com>
2025-06-09 14:32:08 -0300

    Initial commit

--- conteudo/script.js
+++ conteudo/script.js
@@ -7,9 +7,28 @@ let weather=document.getElementById("weather");
 let mensagem_error=document.getElementById("mensagem_error");
 
 bnt_verificar_cidade.addEventListener("click",()=>{
-    const city=bnt_verificar_cidade.ariaValueMax.trim();
+    const city=input_city.value.trim();
 
     if(city == ""){
         mensagem_error.innerHTML="Digite alguma cidade no campo";
     }
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${chave_api}&lang=pt_br&units=metric`)
+        .then(response =>{
+            if(!response.ok) throw new Error("Cidade não encontrada");
+            return response.json();
+        })
+        .then(data => {
+            const name=data.name;
+            const temp=data.main.temp;
+            const description=data.weather[0].description;
+            const icon=data.weather[0].icon;
+
+            cidade.innerHTML=`${name}`;
+            weather.innerHTML=`${temp}°`;
+            
+        })
+            
+        
+
+    
 })
